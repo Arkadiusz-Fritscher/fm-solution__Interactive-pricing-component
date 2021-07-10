c83dcce17e768b8de523b8c6574dfdb01ebca0f7
Arek <arkadiuszp@arcor.de>
2021-07-10 23:20:41 +0200

    first commit

---
+++ README-template.md
@@ -0,0 +1,96 @@
+# Frontend Mentor - Interactive pricing component solution
+
+This is a solution to the [Interactive pricing component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-pricing-component-t0m8PIyY8). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Use the slider and toggle to see prices for different page view numbers
+
+### Screenshot
+
+![Mobile view](./screenshot.jpg)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- JavaScript
+- SASS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+### What I learned
+
+Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
+
+To see how you can add code snippets, see below:
+
+```html
+<h1>Tried to write well-structured and accessibility-friendly code.</h1>
+Styling input elements was something new for me. Through the project, I have
+learned a lot of new things about input elements. As a Tailwind fan, I haven't
+worked with SASS for some time. I learned some new things when it comes to
+splitting sass files or mixins.
+```
+
+```css
+.proud-of-this-css {
+  color: papayawhip;
+}
+```
+
+```js
+<h1>A function should only do one thing.</h1>;
+const init = function () {
+  setSliderAttributes();
+  setPrice;
+
+  // Slider events
+  elements.slider.addEventListener("input", () => {
+    setSliderAttributes();
+    setSliderColor();
+    setPrice();
+  });
+
+  // Checkbox events
+  elements.checkbox.addEventListener("change", () => {
+    calcPriceViews();
+    setPrice();
+  });
+};
+```
+
+### Continued development
+
+I will deal with accessibility even further
+
+## Author
+
+- Website - [Fritscher.dev](https://www.fritscher.dev)
+- Frontend Mentor - [@Arkadiusz-Fritscher](https://www.frontendmentor.io/profile/Arkadiusz-Fritscher)
+- Twitter - [@Fritscher_dev](https://www.twitter.com/fritscher_dev)
---
+++ src/script.js
@@ -0,0 +1,129 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+  const priceList = [
+    { views: 10000, price: 8 },
+    { views: 50000, price: 12 },
+    { views: 100000, price: 16 },
+    { views: 500000, price: 24 },
+    { views: 1000000, price: 36 },
+  ];
+  const data = {
+    minRange: null,
+    maxRange: null,
+    sliderValue: null,
+    isChecked: false,
+    price: null,
+    views: null,
+  };
+
+  /* Dom Elements */
+  const elements = {
+    page: document.querySelector("body"),
+    checkbox: document.querySelector("[type=checkbox]"),
+    slider: document.querySelector("[type=range]"),
+    pageviews: document.querySelector("#views"),
+    price: document.querySelector("#price"),
+  };
+
+  function abbreviateViews(value) {
+    const stringValue = value.toString();
+    const valueLength = stringValue.length - 1;
+    let shorten = "";
+
+    if (stringValue >= 1000) {
+      switch (valueLength) {
+        case 4:
+          shorten = stringValue.substring(0, 2) + "k";
+          break;
+        case 5:
+          shorten = stringValue.substring(0, 3) + "k";
+          break;
+        case 6:
+          shorten = stringValue.substring(0, 1) + "m";
+          break;
+        default:
+          shorten = stringValue.substring(0, 1) + "b";
+          break;
+      }
+    }
+
+    return shorten;
+  }
+
+  const setSliderAttributes = function () {
+    const slider = elements.slider;
+    slider.setAttribute("aria-value", 1);
+    slider.setAttribute("aria-valuemin", 1);
+    slider.setAttribute("aria-valuemax", priceList.length);
+    slider.setAttribute("aria-step", 1);
+    slider.setAttribute("min", 1);
+    slider.setAttribute("max", priceList.length);
+    slider.setAttribute("step", 1);
+    slider.attributes.min.value = 1;
+    slider.attributes.max.value = priceList.length;
+    data.maxRange = slider.attributes.max.value;
+    data.minRange = slider.attributes.min.value;
+    data.sliderValue = slider.value;
+  };
+
+  const checkDiscount = function () {
+    const isChecked = elements.checkbox.checked;
+    data.isChecked = isChecked;
+    return isChecked;
+  };
+
+  const checkDiscountPrice = function (originalPrice) {
+    const isDiscount = checkDiscount();
+    const discountPercent = 25;
+    const discountPrice =
+      originalPrice - ((originalPrice * discountPercent) / 100).toFixed(2);
+    return isDiscount ? discountPrice : originalPrice;
+  };
+
+  const setSliderColor = function () {
+    const { minRange, maxRange, sliderValue } = data;
+    const sliderColor = "hsl(174, 77%, 80%)";
+    const sliderBgColor = "hsl(224, 65%, 95%)";
+    const sliderPercent =
+      ((sliderValue - minRange) / (maxRange - minRange)) * 100;
+    elements.slider.style.background = `linear-gradient(to right, ${sliderColor} 0%, ${sliderColor} ${sliderPercent}%,${sliderBgColor} ${sliderPercent}%, ${sliderBgColor} 100%)`;
+  };
+
+  const calcPriceViews = function () {
+    const sliderValue = data.sliderValue;
+    const { price, views } = priceList[sliderValue - 1];
+    const PRICE = checkDiscountPrice(price).toFixed(2);
+    data.price = PRICE;
+    data.views = views;
+    return { PRICE, views };
+  };
+
+  const setPrice = function () {
+    const sliderValue = data.sliderValue;
+    const { PRICE, views } = calcPriceViews();
+    elements.slider.setAttribute("aria-valuenow", sliderValue);
+    elements.slider.setAttribute("value", sliderValue);
+    elements.pageviews.innerHTML = abbreviateViews(views);
+    elements.price.innerHTML = PRICE;
+  };
+
+  const init = function () {
+    setSliderAttributes();
+    setPrice;
+
+    // Slider events
+    elements.slider.addEventListener("input", () => {
+      setSliderAttributes();
+      setSliderColor();
+      setPrice();
+    });
+
+    // Checkbox events
+    elements.checkbox.addEventListener("change", () => {
+      calcPriceViews();
+      setPrice();
+    });
+  };
+
+  init();
+});
